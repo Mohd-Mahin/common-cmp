a5c9adbe392e4db000f75ded31a4d654b564cb57
Mohd-Mahin <mahinit3016@gmail.com>
2020-08-02 19:20:54 +0530

    build

---
+++ lib/theme/emotion-styled-system.d.ts
@@ -0,0 +1,6 @@
+export declare const Box: any;
+export declare const Flex: any;
+export declare const FlexWrap: any;
+export declare const TInput: any;
+export declare const Spacer: any;
+export declare const TSelect: any;
